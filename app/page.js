6abd41ff09e0a75c5300a03618b072cfc6bba7a6
Dikeledi Mokoatle <2769106@students.wits.ac.za>
2026-08-04 03:36:10 +0200

    feat: create ToDo app page structure and task form

--- app/page.js
+++ app/page.js
@@ -5,6 +5,60 @@ export default function Home(){
     <main className={styles.page}> 
       <div className={styles.todoApp}>
         <h1 className={styles.appTitle}>ToDo App</h1>
+        <p className={styles.welcomeMessage}>Stay Organised, One task at a time.</p>
+
+        <h2 className={styles.actionMessage}> Add A Task </h2>
+
+        <form className={styles.taskForm}>
+          <div className= {styles.inputField}>
+            <label>
+            Title: <input type = "text" placeholder="Please add the title of your task" />
+            </label>
+
+          </div>
+
+          <div className={styles.inputField}>
+            <label>
+            Description: <textarea placeholder="Please add the description of your task" />
+            </label>
+          </div>
+
+          <div className={styles.inputField}>
+            <label>
+            Due Date: <input type="date" placeholder="Please add the due date of your task"/>
+            </label>
+          </div>
+
+
+          <div className={styles.inputField}>
+            <label>
+            Topic: <input type="text" placeholder="Please add the topic related to your task eg. ComSci (Analysis of Algorithms), Arts (Film and Television) etc"/>
+            </label>
+          </div>
+
+
+          <div className={styles.inputField}>
+            
+          <label>
+            Status:
+            <select>
+              <option>Select the status of your task</option>
+              <option>Todo</option>
+              <option>In Progress</option>
+              <option>Complete</option>
+            </select>
+          </label>
+
+          </div>
+
+       
+
+          <button type = "submit" className={styles.addBtn}>
+            Add Task
+          </button>
+
+        </form>
+
       </div>
     </main>
   );
