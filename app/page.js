bb33bc8f00d3a7645e648cc8d5c724ed3e9126f1
Dikeledi Mokoatle <2769106@students.wits.ac.za>
2026-08-04 15:36:00 +0200

    Implement task submission request from frontend to API route

--- app/page.js
+++ app/page.js
@@ -1,6 +1,70 @@
+"use client";
+
+import {useState} from "react";
+
 import styles from "./page.module.css"
 
+
 export default function Home(){
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [topic, setTopic] = useState("");
+  const [due_date, setDueDate] = useState("");
+  const [status, setStatus] = useState("");
+
+  function handleTitleChange(e){
+    setTitle(e.target.value);
+  }
+
+  function handleDescriptionChange(e){
+    setDescription(e.target.value);
+  }
+
+  function handleTopicChange(e){
+    setTopic(e.target.value);
+  }
+
+  function handleDueDateChange(e){
+    setDueDate(e.target.value);
+  }
+
+  function handleStatusChange(e){
+    setStatus(e.target.value);
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault();
+
+    const task = {
+      title: title,
+      description: description,
+      due_date: due_date,
+      topic: topic,
+      status: status
+    };
+
+    const response = await fetch("/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(task)
+    });
+
+    const data = await response.json();
+
+    if (response.ok){
+      alert(data.message);
+      window.location.reload();
+    }
+    else{
+      alert("Failed to add task!");
+    }
+  }
+
+
+
   return(
     <main className={styles.page}> 
       <div className={styles.todoApp}>
@@ -9,30 +73,58 @@ export default function Home(){
 
         <h2 className={styles.actionMessage}> Add A Task </h2>
 
-        <form className={styles.taskForm}>
+        <form 
+          className={styles.taskForm}
+          onSubmit={handleSubmit}
+        
+        >
           <div className= {styles.inputField}>
             <label>
-            Title: <input type = "text" placeholder="Please add the title of your task" />
+            Title: 
+            <input 
+              type = "text" 
+              placeholder="Please add the title of your task" 
+              value = {title}
+              onChange = {handleTitleChange}
+            />
             </label>
 
           </div>
 
           <div className={styles.inputField}>
             <label>
-            Description: <textarea placeholder="Please add the description of your task" />
+            Description: 
+            <textarea 
+              placeholder="Please add the description of your task" 
+              value = {description}
+              onChange={handleDescriptionChange}
+            />
+
             </label>
           </div>
 
           <div className={styles.inputField}>
             <label>
-            Due Date: <input type="date" placeholder="Please add the due date of your task"/>
+            Due Date: 
+            <input 
+              type="date" 
+              placeholder="Please add the due date of your task"
+              value = {due_date}
+              onChange={handleDueDateChange}  
+            />
             </label>
           </div>
 
 
           <div className={styles.inputField}>
             <label>
-            Topic: <input type="text" placeholder="Please add the topic related to your task eg. ComSci (Analysis of Algorithms), Arts (Film and Television) etc"/>
+            Topic: 
+            <input 
+              type="text" 
+              placeholder="Please add the topic related to your task eg. ComSci (Analysis of Algorithms), Arts (Film and Television) etc"
+              value={topic}
+              onChange={handleTopicChange}  
+            />
             </label>
           </div>
 
@@ -41,8 +133,11 @@ export default function Home(){
             
           <label>
             Status:
-            <select>
-              <option>Select the status of your task</option>
+            <select
+              value = {status}
+              onChange = {handleStatusChange}
+            >
+              <option>Select the status of your task </option>
               <option>Todo</option>
               <option>In Progress</option>
               <option>Complete</option>
@@ -63,3 +158,5 @@ export default function Home(){
     </main>
   );
 }
+
+
